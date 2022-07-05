4ce6eb9b3e0e9f72e5044f10b1eae6871639e58e
krisamorim <kris.a.furtado@Gmail.com>
2022-07-05 19:59:25 -0300

    Separei os tipos primitivos

--- Aula0002-TiposEvariaves/src/aula002.java
+++ Aula0002-TiposEvariaves/src/aula002.java
@@ -3,12 +3,16 @@ import java.math.BigDecimal;
 public class aula002 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//tipos primitivos
 		byte tipoByte = 0;
 		short tipoShort = 0;
 		int tipoInt = 0;
+		long tipoLong = 58859;
+		
+		
 		boolean tipoBoolean = true;
 		String tipoTexto = "texto";
+		
 		//big decimal pe um classe por isso as variáveis desse tipo são criadas diferentes
 		BigDecimal big1 = new BigDecimal("0.1");
         BigDecimal big2 = new BigDecimal("0.2");
