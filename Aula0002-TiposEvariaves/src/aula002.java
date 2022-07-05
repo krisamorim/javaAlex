43794ad1e778fc061f78fbc55650567b6f92e017
krisamorim <kris.a.furtado@Gmail.com>
2022-07-05 18:49:24 -0300

    Tipods de variáveis

--- Aula0002-TiposEvariaves/src/aula002.java
+++ Aula0002-TiposEvariaves/src/aula002.java
@@ -1,9 +1,20 @@
+import java.math.BigDecimal;
 
 public class aula002 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		byte tipoByte = 0;
+		short tipoShort = 0;
+		int tipoInt = 0;
+		boolean tipoBoolean = true;
+		String tipoTexto = "texto";
+		//big decimal pe um classe por isso as variáveis desse tipo são criadas diferentes
+		BigDecimal big1 = new BigDecimal("0.1");
+        BigDecimal big2 = new BigDecimal("0.2");
+        //essa pe a forma como somamos numeros em bigDecimal
+        BigDecimal bigResult = big1.add(big2);
+        System.out.println(bigResult.toString());
 	}
 
 }
