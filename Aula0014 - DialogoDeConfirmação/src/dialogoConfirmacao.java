f2a79644186231dd0fe82e010d9543479200280e
krisamorim <kris.a.furtado@Gmail.com>
2022-07-09 11:28:26 -0300

    Usando showConfirm - Dialogo apra confirmação

--- Aula0014 - DialogoDeConfirmação/src/dialogoConfirmacao.java
+++ Aula0014 - DialogoDeConfirmação/src/dialogoConfirmacao.java
@@ -1,8 +1,18 @@
+import javax.swing.JOptionPane;
 
 public class dialogoConfirmacao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Calculadora");
+		double num1 = Double.parseDouble(JOptionPane.showInputDialog("Qual o primeiro número?"));
+		double num2 = Double.parseDouble(JOptionPane.showInputDialog("Qual o segund número?"));
+		//int opcao = Integer.parseInt(JOptionPane.showInputDialog("O que deseja fazer?\n1 p/ Somar\n2 p/ Subtrair\n3 p/ multiplicar\n4 p/ Dividir"));
+		int confirma = JOptionPane.showConfirmDialog(null, "Deseja somar?");
+		if(confirma == 1 || confirma == 2) {
+			JOptionPane.showMessageDialog(null, "Até logo.");
+		}else {
+			JOptionPane.showMessageDialog(null, "A soma de "+num1+" + "+num2+" é "+(num1+num2));
+		}
 
 	}
 
