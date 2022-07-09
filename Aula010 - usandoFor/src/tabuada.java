cfc59b84a3a94ef070bcbc543382f04d076bfb60
krisamorim <kris.a.furtado@Gmail.com>
2022-07-08 21:55:20 -0300

    Código bonus que mostra como gera numero aleatório

--- Aula010 - usandoFor/src/tabuada.java
+++ Aula010 - usandoFor/src/tabuada.java
@@ -1,8 +1,9 @@
+import java.util.Random;
 import javax.swing.JOptionPane;
 
 public class tabuada {
 
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		//perguntar qual tabuada deseja
 		int num1 = Integer.parseInt(JOptionPane.showInputDialog("Qual tabuada você Deseja?"));
 		System.out.println("Número escolhido foi "+num1);
@@ -12,6 +13,17 @@ public class tabuada {
 			//imprimir no console
 			System.out.println(num1+" x "+i+" = "+(num1*i));
 		}
+		criaNumAleatorio();
 	}
-
+	
+	//código bonus
+	public static void criaNumAleatorio() {
+		Random gerador = new Random();
+		System.out.println("\nRodando codigo bonus");
+		
+		//gerando 10 numeros aleatórios de 0 a 50
+		for (int i = 0; i < 10; i++) {
+			System.out.println(gerador.nextInt(50));
+	    }
+	}
 }
