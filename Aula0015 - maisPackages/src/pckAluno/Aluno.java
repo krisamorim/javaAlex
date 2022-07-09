4d3cb86f8c49409ab00f7b46d88a0ba0d8782d7a
krisamorim <kris.a.furtado@Gmail.com>
2022-07-09 12:39:23 -0300

    criando dois pacotes

---
+++ Aula0015 - maisPackages/src/pckAluno/Aluno.java
@@ -0,0 +1,14 @@
+package pckAluno;
+
+public class Aluno {
+	String nome;
+	int idade;
+	String dataNascimento;
+	String rg;
+	String cpf;
+	String mae;
+	String pai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculada;
+}
