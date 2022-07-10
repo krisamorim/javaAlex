6f0bd75e517c545083eda2fdc7b1064a4b97e22e
krisamorim <kris.a.furtado@Gmail.com>
2022-07-10 12:49:19 -0300

    Arquivo de teste: testando comunicação entre pacotes, utilizando matodos piblicos

---
+++ 0-testesAleatórios/src/pckgSecundario/Xpto.java
@@ -0,0 +1,22 @@
+package pckgSecundario;
+
+public class Xpto {
+	
+	public String name;
+	public int yearsOld;
+	String pai;
+	String mae;
+
+	public Xpto() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Xpto(String nameDefault) {
+		name = nameDefault;
+	}
+	
+	public Xpto (String nameDefault, int yearsOldDefault) {
+		name = nameDefault;
+		yearsOld = yearsOldDefault;
+	}
+}
