cbff227e1e2cf8b35b7e31205a7932f2a709f152
krisamorim <kris.a.furtado@Gmail.com>
2022-07-16 16:02:31 -0300

    before creating the hashCode

--- Aula0021 - EqualsAndHashCode/src/EqualAndHashcode.java
+++ Aula0021 - EqualsAndHashCode/src/EqualAndHashcode.java
@@ -5,7 +5,7 @@ public class EqualAndHashcode {
 	public static void main(String[] args) {
 		//beginning student class
 		class Student{
-			String name;
+			String name, cpf;
 			int id;
 			double grade1, grade2, grade3, grade4;
 			
@@ -13,15 +13,20 @@ public class EqualAndHashcode {
 			public String getName() {
 				return name;
 			}
-
 			public void setName(String name) {
 				this.name = name;
 			}
-
+			
+			public String getCpf() {
+				return cpf;
+			}
+			public void setCpf(String cpf) {
+				this.cpf = cpf;
+			}
+			
 			public int getId() {
 				return id;
 			}
-
 			public void setId(int id) {
 				this.id = id;
 			}
@@ -65,26 +70,45 @@ public class EqualAndHashcode {
 			public String getStatus() {
 				return getAverage() > 7 ? "aproved" : getAverage() > 5.5 ? "retake test" : "flunked";
 			}
+
+			//ToString
+			@Override
+			public String toString() {
+				return "Student [name=" + name + ", id=" + id + ", getCpf()=" + getCpf() + ", grade1=" + grade1 + ", grade2=" + grade2
+						+ ", grade3=" + grade3 + ", grade4=" + grade4 + ", getAverage()=" + getAverage()
+						+ ", getStatus()=" + getStatus() + "]";
+			}
+			
+			
 		}
 		//end of Student class
 		
-		//instance student1 variable
+		//instance student1 and 2 variables
 		Student student1 = new Student();
 		Student student2 = new Student();
+		
 		Boolean checkEquals = student1.equals(student2);
-		//get the name of the first student
+		
+		//set the name of the first student
 		student1.setName("Anna");
-		//get the name of the second student
+		//set the name of the second student
 		student2.setName("Anna");
 		Boolean checkEqualsName = student1.name.equals(student2.name);
 		
+		//set the cpf of the first student
+		student1.setCpf("123456789");
+		//set the cpf of the second student
+		student2.setCpf("123456788");
+		Boolean checkEqualsCPF = student1.cpf.equals(student2.cpf);
 		
 				
 		//show in the console
+		//the object comparing is false because java check the memory position. even if all the data were the same the result would be false
 		System.out.println("-----------------Comparing objects-----------------\nstudent1: "+student1+"\nstudent2: "+student2+"\nEquals: "+checkEquals);
-		//the object comparing is false because java check the memory position
 		
+		//the object comparing now is true because in the code below the NAME are comparing
 		System.out.println("\n-------------Comparing objects' names--------------\nstudent.name: "+student1.name+"\nstudent2.name: "+student2.name+"\nEquals: "+checkEqualsName);
+		
 	}
 
 }
