43e148c6a6ee18fd98dd73b01e9af42231a65d75
krisamorim <kris.a.furtado@Gmail.com>
2022-07-17 22:27:41 -0300

    Creating an array

---
+++ Aula0023 - ListArray/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//a way to create an array
+		int n[] = {3,5,1,2,6,4};
+		
+		
+	}
+
+}
