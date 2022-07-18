6e65ecc85bc7603275764163950f5e34c180da1d
krisamorim <kris.a.furtado@Gmail.com>
2022-07-17 22:30:26 -0300

    Creation array named num with 5 position and fill all positions with number 2 and then print in console

--- Aula0023 - ListArray/src/main/Main.java
+++ Aula0023 - ListArray/src/main/Main.java
@@ -10,7 +10,15 @@ public class Main {
 		//a way to create an array
 		int n[] = {3,5,1,2,6,4};
 		
-		
+		//creating an array with 5 position
+		int num[] = new int[5];
+		//fill all position with number 2
+		Arrays.fill(num, 2);
+		//show array
+		for (int valores: num) {
+			System.out.println(valores);
+		}
+								
 	}
 
 }
