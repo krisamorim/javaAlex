00515086206d6f5858d98ab4c19899e6d9650070
krisamorim <kris.a.furtado@Gmail.com>
2022-07-20 17:04:59 -0300

    ListArray - Get the student's data

--- Aula0024 - ListArrayWithObject/src/main/Main.java
+++ Aula0024 - ListArrayWithObject/src/main/Main.java
@@ -1,4 +1,6 @@
 package main;
+import javax.swing.JOptionPane;
+
 import classes.*;
 
 public class Main {
@@ -6,6 +8,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Student student1 = new Student();
+		student1.setName(JOptionPane.showInputDialog("What's the student's name?"));
+		student1.setPeopleRegister(JOptionPane.showInputDialog("What's the student's people register?"));
+		student1.setId(Integer.parseInt(JOptionPane.showInputDialog("What's the student's ID?")));
 	}
 
 }
