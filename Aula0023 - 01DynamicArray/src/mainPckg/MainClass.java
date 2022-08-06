bd14b4f3251d3435fd99e9b6e1956c864957fb27
krisamorim <kris.a.furtado@Gmail.com>
2022-08-06 14:50:13 -0300

    commenting out MainClass file code

--- Aula0023 - 01DynamicArray/src/mainPckg/MainClass.java
+++ Aula0023 - 01DynamicArray/src/mainPckg/MainClass.java
@@ -5,17 +5,25 @@ import java.util.ArrayList;
 public class MainClass {
 
 	public static void main(String[] args) {
+		
+		//creating a new Array list called users
 		List<User> users = new ArrayList<>();
 
+		//for to creating 10 users' name like "Nome+number" and last name with "Sobrenone + number"  
 		for(int i=0; i<10; i++) {
+			
+			//creating a new variable to receive the name
 			User current = new User("Nome" + i, "Sobrenome" + i);
+			
+			//adding the new name in to the users variable
 			users.add(current);
 		}
 		
+		//print in the console all users from users variable, one by one
 		for (User usuario: users) {
 			System.out.println(usuario);
 		}
-		
+		//print all users of the users variable, in a single print
 		System.out.println(users);
 	}
 
