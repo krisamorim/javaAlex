47dc3f16759c25dad76fb4ec23ffd0ccb6707369
krisamorim <kris.a.furtado@Gmail.com>
2022-08-06 18:36:24 -0300

    a

--- Aula0024 - ListArrayWithObject/src/main/Main.java
+++ Aula0024 - ListArrayWithObject/src/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Student student1 = new Student();
-				student1.setName(JOptionPane.showInputDialog("What's the student's name?"));
+		student1.setName(JOptionPane.showInputDialog("What's the student's name?"));
 		student1.setPeopleRegister(JOptionPane.showInputDialog("What's the student's people register?"));
 		student1.setId(Integer.parseInt(JOptionPane.showInputDialog("What's the student's ID?")));
 		
