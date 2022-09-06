8fe7c901ddc505db27853d25a753156ff8c1f561
krisamorim <kris.a.furtado@Gmail.com>
2022-09-05 22:00:39 -0300

    Discipline object finished.

--- Aula0028 - StudentList/src/obj/pkg/Discipline.java
+++ Aula0028 - StudentList/src/obj/pkg/Discipline.java
@@ -1,5 +1,46 @@
 package obj.pkg;
 
-public class Discipline {
+import java.util.Objects;
 
+public class Discipline {
+	String name;
+	double grade;
+	
+	//setter and getters-------------------------------------------
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getGrade() {
+		return grade;
+	}
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
+	
+	//hascode and equals------------------------------------------
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Discipline other = (Discipline) obj;
+		return Objects.equals(name, other.name);
+	}
+	
+	//ToString-----------------------------------------------------
+	@Override
+	public String toString() {
+		return "Discipline [name=" + name + ", grade=" + grade + "]";
+	}
+	
 }
