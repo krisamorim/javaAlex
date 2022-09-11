723f21d609148ffb615c1e5f75c954e1b5e2d598
krisamorim <kris.a.furtado@Gmail.com>
2022-09-11 00:13:33 -0300

    get the sutdent data and set it

--- Aula0028 - StudentList/src/exec/pkg/Exec.java
+++ Aula0028 - StudentList/src/exec/pkg/Exec.java
@@ -1,5 +1,18 @@
 package exec.pkg;
+import javax.swing.JOptionPane;
+
+import obj.pkg.Student;
 
 public class Exec {
-
+	public static void main(String[] args) {
+		Student student1 = new Student();
+		
+		student1.setName(JOptionPane.showInputDialog("What's the student name?"));
+		student1.setCpf(JOptionPane.showInputDialog("What's the student Individual Registration?"));
+		student1.setId(Integer.parseInt(JOptionPane.showInputDialog("What's the student ID?")));
+		
+		System.out.println("Student data without discipline data: " + student1);
+		
+		
+	}
 }
